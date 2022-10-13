e4fc3c8082eed33130efdd5a789761364696f16e
Jorge Ruiz <jorge.ruiz@undostres.com.mx>
2022-10-13 16:15:33 -0500

    guardando llaves y vista de login util

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -13,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -30,16 +22,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -48,68 +31,214 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+   static const Color primaryColor = Color(0xFF13B5A2);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+   // Create storage
+  final _storage = const FlutterSecureStorage();
+
+  final TextEditingController _usernameController =
+      TextEditingController(text: "");
+  final TextEditingController _passwordController =
+      TextEditingController(text: "");
+
+  bool passwordHidden = true;
+  bool _savePassword = true;
+
+  // Read values
+  Future<void> _readFromStorage() async {
+    _usernameController.text = await _storage.read(key: "KEY_USERNAME") ?? '';
+    _passwordController.text = await _storage.read(key: "KEY_PASSWORD") ?? '';
+  }
+
+  _onFormSubmit() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      if (_savePassword) {
+        // Write values
+        await _storage.write(key: "KEY_USERNAME", value: _usernameController.text);
+        await _storage.write(key: "KEY_PASSWORD", value: _passwordController.text);
+      }
+    }
+  }
+
+  _onForgotPassword() {}
+
+  _onSignUp() {}
+
+  @override
+  void initState() {
+    super.initState();
+    _readFromStorage();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _usernameController.dispose();
+    _passwordController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      backgroundColor: Colors.white,
+      key: _scaffoldKey,
+      body: SingleChildScrollView(
+        child: Container(
+          width: size.width,
+          padding: EdgeInsets.all(size.width - size.width * .85),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: size.height * .10,
+              ),
+              const Text(
+                "Storage data",
+                style: TextStyle(
+                    color: Color(0xFF161925),
+                    fontWeight: FontWeight.w600,
+                    fontSize: 32),
+              ),
+              SizedBox(
+                height: size.height * .15,
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Username",
+                        labelStyle: const TextStyle(color: primaryColor),
+                        focusedBorder: UnderlineInputBorder(borderRadius: BorderRadius.circular(0), borderSide: const BorderSide(color: primaryColor, width: 2),),
+                      ),
+                      controller: _usernameController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Required field';
+                        }
+                        return null;
+                      },
+                      textInputAction: TextInputAction.next,
+                      textCapitalization: TextCapitalization.none,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    SizedBox(
+                      height: size.height * .02,
+                    ),
+                    TextFormField(
+                      textInputAction: TextInputAction.done,
+                      validator: (value) {
+                        return value!.isEmpty ? "Required field" : null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: const TextStyle(color: Color(0xFF95989A)),
+                        focusedBorder: UnderlineInputBorder(borderRadius: BorderRadius.circular(0), borderSide: const BorderSide(color: primaryColor, width: 2),),
+                        suffixIcon: InkWell(
+                          onTap: () {
+                            setState(() {
+                              passwordHidden = !passwordHidden;
+                            });
+                          },
+                          child: Icon(
+                            passwordHidden
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined,
+                            color: const Color(0xff747881),
+                            size: 23,
+                          ),
+                        ),
+                      ),
+                      controller: _passwordController,
+                      obscureText: passwordHidden,
+                      enableSuggestions: false,
+                      toolbarOptions: const ToolbarOptions(
+                        copy: false,
+                        paste: false,
+                        cut: false,
+                        selectAll: false,
+                        //by default all are disabled 'false'
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: size.height * .045,
+              ),
+              CheckboxListTile(
+                value: _savePassword,
+                onChanged: (bool? newValue) {
+                  setState(() {
+                    _savePassword = newValue!;
+                  });
+                },
+                title: const Text("Remember me"),
+                activeColor: primaryColor,
+              ),
+              SizedBox(
+                height: size.height * .05,
+              ),
+              SizedBox(
+                width: size.width,
+                child: ElevatedButton(
+                  onPressed: _onFormSubmit,
+                  child: const Text("Sign In"),
+                  style: ElevatedButton.styleFrom(
+                    primary: primaryColor,
+                    textStyle: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold)
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height * .015,
+              ),
+              Center(
+                child: InkWell(
+                  onTap: _onForgotPassword,
+                  child: const Text(
+                    "Forgot password?",
+                    style: TextStyle(color: Colors.black54, fontSize: 14),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height * .035,
+              ),
+              const Center(
+                child: Text(
+                  "You don't have an account?",
+                  style: TextStyle(fontSize: 14, ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: size.height * .01,
+              ),
+              Center(
+                child: InkWell(
+                  onTap: _onSignUp,
+                  child: const Text(
+                    "Sign Up now to get access.",
+                    style: TextStyle( color: primaryColor,fontSize: 16, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
